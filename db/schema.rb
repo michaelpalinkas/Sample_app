bcf27c8fa09f13de7a4f3bbafe36738de3e3b53f
Michael Palinkas <michael.palinkas@uconn.edu>
2013-10-24 15:36:57 -0400

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131013234739) do
+ActiveRecord::Schema.define(version: 20131017201556) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -20,16 +20,16 @@ ActiveRecord::Schema.define(version: 20131013234739) do
     t.datetime "updated_at"
   end
 
-  add_index "products", ["name"], name: "index_products_on_name", unique: true
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
