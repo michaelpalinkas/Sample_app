efd79cd1dbf6ee83e07f75abaf5958b1990e3308
Michael Palinkas <michael.palinkas@uconn.edu>
2013-11-03 17:57:54 -0500

    Finish suer edit, update, index and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017201556) do
+ActiveRecord::Schema.define(version: 20131103222032) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20131017201556) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
